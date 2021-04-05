2892bc5fb3a5f58c58a95ce370f44c0cb3342131
jidkiy-alt <76746846+jidkiy-alt@users.noreply.github.com>
2021-04-05 20:03:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,44 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len-1; i++) {
+    for (int j = i+1; j < len; j++) {
+      if (arr[i] + arr[j] == value)
+        count++;
+    }
+  }
+  return count;
 }
 
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = len-1; i > -1; i--) {
+    for (int j = 0; j != i; ++j) {
+      if (arr[i] + arr[j] == value)
+        count++;
+    }
+  }
+  return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int leftS = 0, count = 0, mid = 0;
+  int rightS = len-1;
+  for (int i = 0; i < len/2; i++) {
+    while (leftS <= rightS) {
+      mid = (leftS + rightS) / 2;
+      if (arr[i] + arr[mid] == value) {
+        count++;
+        leftS = mid-1;
+      }
+      if (arr[i] + arr[mid] > value)
+        rightS = mid-1;
+      if (arr[i] + arr[mid] < value)
+        leftS = mid+1;
+    }
+    mid = (leftS + rightS) / 2;
+    leftS = 0; rightS = len-1;
+  }
+  return count;
 }
