afbd5ddcd20da1ee72783329c4f06ce6d113917f
jidkiy-alt <76746846+jidkiy-alt@users.noreply.github.com>
2021-04-09 19:03:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,22 +23,37 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int leftS = 0, count = 0, mid = 0;
+  int leftS = 0, count = 0, mid = 0, i = 0;
   int rightS = len-1;
-  for (int i = 0; i < len/2; i++) {
-    while (leftS <= rightS) {
-      mid = (leftS + rightS) / 2;
-      if (arr[i] + arr[mid] == value) {
-        count++;
-        leftS = mid-1;
+  bool midCenter = true, rightPos = false;
+  while (leftS <= rightS && arr[i] <= value/2) {
+    if (midCenter)
+      mid = (leftS+rightS)/2;
+    if (arr[i] + arr[mid] == value || !midCenter) {
+      if (arr[mid+1] + arr[i] == value && !rightPos) {
+        midCenter = false;
+        mid++;
+      } else if (arr[mid-1] + arr[i] == value) {
+        rightPos = true; midCenter = false;
+        mid--;
+        if (i != mid && i < mid)
+          count++;
+      } else {
+        rightPos = false; midCenter = true;
+        if (i != mid)
+          count++;
+        leftS = 0; rightS = len-1;
+        i++;
       }
-      if (arr[i] + arr[mid] > value)
-        rightS = mid-1;
-      if (arr[i] + arr[mid] < value)
-        leftS = mid+1;
     }
-    mid = (leftS + rightS) / 2;
-    leftS = 0; rightS = len-1;
+    if (arr[i] + arr[mid] > value)
+      rightS = mid-1;
+    if (arr[i] + arr[mid] < value)
+      leftS = mid+1;
+    if (leftS > rightS) {
+      leftS = 0; rightS = len-1;
+      i++;
+    }
   }
   return count;
 }
