31e86caa42de989f84f9b591e58f19b215ba8d73
jidkiy-alt <76746846+jidkiy-alt@users.noreply.github.com>
2021-04-11 17:27:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,10 +13,12 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  for (int i = len-1; i > -1; i--) {
-    for (int j = 0; j != i; ++j) {
-      if (arr[i] + arr[j] == value)
-        count++;
+  for(int i = len-1; i > -1; i--) {
+    if (arr[i] <= value) {
+      for(int j = 0; j != i; ++j) {
+        if (arr[i] + arr[j] == value)
+          count++;
+      }
     }
   }
   return count;
